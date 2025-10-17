1886f5e6286d11b391d86fec3c8cd77047a72e18
Unnecessary. <55099698+Yannick-A@users.noreply.github.com>
2025-10-17 03:44:39 +0200

    Accidentally committing secrets

--- app.py
+++ app.py
@@ -1,1 +1,3 @@
-AWS_SECRET_ACCESS_KEY=AKIAIOSFODNN7EXAMPLE
+# fake demo secrets – these are *not* real keys
+AWS_SECRET_ACCESS_KEY="AKIAIOSFODNN7EXAMPLE"
+GITHUB_TOKEN="ghp_1234567890abcdefghijklmnopqrstuv"
