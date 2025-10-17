239e3e7def79eb0b79dc45c916e60d944d01754c
Unnecessary. <55099698+Yannick-A@users.noreply.github.com>
2025-10-17 03:48:10 +0200

    Trigger secret detection

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
 # fake demo secrets – these are *not* real keys
 AWS_SECRET_ACCESS_KEY="AKIAIOSFODNN7EXAMPLE"
 GITHUB_TOKEN="ghp_1234567890abcdefghijklmnopqrstuv"
+NEW_GITHUB_TOKEN="ghp_DEMOSECRET1234567890abcdefghijklm"
